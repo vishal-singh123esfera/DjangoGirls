f283250f8d48da2f7fb052c534c3c97b9e13f826
vishal-singh123esfera <vishal_singh@esferasoft.com>
2017-07-06 17:51:46 +0530

    Create Form URL:pos/new

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from .form	import PostForm
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {})
+
+def	post_new(request):
+	form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form':form})
+

9d9c07bd4b0908b0600cefa24b2afb16e65f961f
vishal-singh123esfera <vishal_singh@esferasoft.com>
2017-07-07 18:38:11 +0530

    Create Post with edit button

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
 from .form	import PostForm
-from	django.shortcuts	import	redirect
+from django.shortcuts import redirect
 
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -34,3 +34,7 @@ def	post_edit(request,	pk):
 	else:
 		form	=	PostForm(instance=post)
 	return	render(request,	'blog/post_edit.html',	{'form':	form})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
